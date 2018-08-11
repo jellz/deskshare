85c169b65398207c13c89c9792556e522388d740
RONTheCookie <ronthecookie0101@gmail.com>
2018-08-11 17:27:03 +0300

    git is a bad boy and wont let me merge if i have untracked changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const fs = require('fs');
+// const config = require('./config.json');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 const r = module.exports.r = require('rethinkdbdash')({ db: 'deskshare' });
-const jwtKey = module.exports.jwtKey = require('fs').readFileSync('jwtRS256.key').toString();
+const jwtKey = module.exports.jwtKey = fs.readFileSync('jwt.key').toString();
 
 app.use(express.json());
 app.use(require('morgan')('dev'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const config = require('./config.json');
 const router = express.Router();
+
+const githubOAuth = require('github-oauth')({
+    githubClient: config.githubClient,
+    githubSecret: config.githubSecret,
+    baseURL: config.baseURL,
+    loginURI: '/api/auth/login',
+    callbackURI: '/api/auth/callback',
+    scope: 'user' // optional, default scope is set to user
+});
 
 router.get('/', async (req, res) => {
     res.send('Hello, world!');

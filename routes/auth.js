3b20a1681cefc16083620fbdb35b8f787aa0765c
Jellz <xdjellz@gmail.com>
2018-08-12 00:23:30 +1000

    export route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = module.exports = express.Router();
 
 router.get('/', async (req, res) => {
     res.send('Hello, world!');

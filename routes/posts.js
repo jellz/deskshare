14331ff33af4ac00fc18de63ff9296ba0f8e3aac
Jellz <xdjellz@gmail.com>
2018-08-12 03:46:05 +1000

    new post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = module.exports = express.Router();
+const r = require('../index.js').r;
+const { handleJoi } = require('../util.js');
 
 router.post('/', async (req, res) => {
-    res.send('')
+    if (!req.user) return;
+    if (!await handleJoi(require('../schemas/NewPost.js'), req, res)) return;
+    await r.table('posts').insert({
+        title: req.body.title,
+        description: req.body.description,
+        images: req.body.images
+    });
 });
---
+++ util.js
@@ -0,0 +1,12 @@
+exports.handleJoi = (schema, req, res) => {
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        if (!result.error.isJoi) {
+            console.error(`Error while running Joi at ${Date.now()}: ${result.error}`);
+            res.sendStatus(500);
+            return false;
+        }
+        res.status(400).json({ error: result.error.name, details: result.error.details.map(item => item.message) });
+        return false;
+    } else return true;
+}

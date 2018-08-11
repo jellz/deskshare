15013fac4d15ee2d005668c988e294c9f614d03d
Robert Westbury <robert-spam@miegel.org>
2018-08-11 15:20:39 +0100

    install ui lib (keen ui)

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import KeenUI from 'keen-ui';
+import 'keen-ui/dist/keen-ui.css';
+
+Vue.use(KeenUI);
+
 Vue.config.productionTip = false
 
 new Vue({

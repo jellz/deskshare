f32c91e9ca50e40f8364cc1540e5868e8667f892
Robert Westbury <robert-spam@miegel.org>
2018-08-11 15:58:55 +0100

    Navbar improvements

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,19 +1,33 @@
 <template>
-<ui-toolbar brand="DeskShare" title="">
-    <div slot="actions">
-      <ui-button color="primary" :size="size">Login/Signup</ui-button>
-    </div>
-</ui-toolbar>
+  <ui-toolbar brand="DeskShare" title="">
+      <div slot="actions">
+
+        <div v-if="loggedIn === true">
+          <ui-button color="primary" :size="size">Submit New Setup</ui-button>
+
+          <ui-button color="primary" :size="size" class="mysetups">My Setups</ui-button>
+        </div>
+        <div v-else>
+          <ui-button color="primary" :size="size">Login/Signup</ui-button>
+        </div>
+      </div>
+  </ui-toolbar>
 </template>
 
 <script>
 import * as store from '../store';
 
 export default {
-
+  data() {
+    return {
+      loggedIn: false
+    };
+  },
+  mounted() {
+    this.loggedIn = store.isLoggedIn();
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 </style>
--- index.js
+++ index.js
@@ -5,15 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-const r = (module.exports.r = require('rethinkdbdash')({ db: 'deskshare' }));
-const jwtKey = (module.exports.jwtKey = require('fs')
-  .readFileSync('jwtRS256.key')
-  .toString());
-=======
 const r = module.exports.r = require('rethinkdbdash')({ db: 'deskshare' });
 const jwtKey = module.exports.jwtKey = require('fs').readFileSync('jwt.key').toString();
->>>>>>> 94fb0b5889da856af85b0ea8a7938de474d318fb
 
 app.use(express.json());
 app.use(require('morgan')('dev'));

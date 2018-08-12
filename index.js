f653144ca3d0489f48242109c1774fdb218a9562
Jellz <xdjellz@gmail.com>
2018-08-12 19:48:26 +1000

    some code spacing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const r = module.exports.r = require('rethinkdbdash')({ db: 'deskshare' });
 const jwtKey = module.exports.jwtKey = require('fs').readFileSync('jwt.key').toString();
 
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:1373'}))
+app.use(cors({ origin: 'http://localhost:1373' }));
 app.use(require('morgan')('dev'));
 
 app.use(ejwt({ secret: jwtKey, credentialsRequired: false }), async (req, res, next) => {

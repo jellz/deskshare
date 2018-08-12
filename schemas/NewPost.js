bd0f0bbbaaf9eb7d8a3f9c5a87c216a680dacb7e
Jellz <xdjellz@gmail.com>
2018-08-12 15:45:12 +1000

    remove comment about primary image; the first image in the array is the primary image

--- schemas/NewPost.js
+++ schemas/NewPost.js
@@ -4,5 +4,4 @@ module.exports = Joi.object().required().keys({
     title: Joi.string().max(35).min(2).required(),
     description: Joi.string().max(280).required(),
     images: Joi.array().min(1).max(15).items(Joi.string().uri().required()).required(),
-    // primaryImage: Joi.string().uri().required() If we want to add a way for people to select which image shows on a thumbnail or something
 });

72e09efd49c8e44850688e3768c7fbc782d970e1
Jellz <xdjellz@gmail.com>
2018-08-12 17:16:17 +1000

    who am I?

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = module.exports = express.Router();
 const r = require('../index.js').r;
 
+router.get('/@me', async (req, res) => {
+    if (!req.user) return res.sendStatus(401);
+    res.json({ me: await r.table('users').get(parseInt(req.user)).run() });
+});
+
 router.get('/:id/posts', async (req, res) => {
     if (!req.params.id) return res.sendStatus(400);
     const user = await r.table('users').get(parseInt(req.params.id)).run();
